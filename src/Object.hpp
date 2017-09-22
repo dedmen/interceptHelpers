910450376dfaecd1da81b5a014f13202506f7002
Zakant <Zakant@gmx.de>
2017-09-22 12:33:41 +0200

    Fixed access modifier and calls

--- src/Object.hpp
+++ src/Object.hpp
@@ -10,74 +10,74 @@ namespace intercept
 	{
 		class Object
 		{
-		private:
+		protected:
 			object _obj;
 		public:
 			void setPos(vector3 vec)
 			{
-				sqf::set_pos(_obj, vec);
+				sqf::set_pos(this->_obj, vec);
 			}
 			void setPosAsl(vector3 vec)
 			{
-				sqf::set_pos_asl(_obj, vec);
+				sqf::set_pos_asl(this->_obj, vec);
 			}
 			void setPosAsl2(vector3 vec)
 			{
-				sqf::set_pos_asl2(_obj, vec);
+				sqf::set_pos_asl2(this->_obj, vec);
 			}
 			void setPosAslw(vector3 vec)
 			{
-				sqf::set_pos_aslw(_obj, vec);
+				sqf::set_pos_aslw(this->_obj, vec);
 			}
 			void setPosAtl(vector3 vec)
 			{
-				sqf::set_pos_atl(_obj, vec);
+				sqf::set_pos_atl(this->_obj, vec);
 			}
 			void setPosWorld(vector3 vec)
 			{
-				sqf::set_pos_world(_obj, vec);
+				sqf::set_pos_world(this->_obj, vec);
 			}
 
 			vector3 getPos()
 			{
-				return sqf::get_pos(_obj);
+				return sqf::get_pos(this->_obj);
 			}
 			vector3 getPosAsl()
 			{
-				return sqf::get_pos_asl(_obj);
+				return sqf::get_pos_asl(this->_obj);
 			}
 			vector3 getPosAslVisual()
 			{
-				return sqf::get_pos_asl_visual(_obj);
+				return sqf::get_pos_asl_visual(this->_obj);
 			}
 			vector3 getPosAslw()
 			{
-				return sqf::get_pos_aslw(_obj);
+				return sqf::get_pos_aslw(this->_obj);
 			}
 			vector3 getPosAtl()
 			{
-				return sqf::get_pos_atl(_obj);
+				return sqf::get_pos_atl(this->_obj);
 			}
 			vector3 getPosAtlVisual()
 			{
-				return sqf::get_pos_atl_visual(_obj);
+				return sqf::get_pos_atl_visual(this->_obj);
 			}
 			vector3 getPosVisual()
 			{
-				return sqf::get_pos_visual(_obj);
+				return sqf::get_pos_visual(this->_obj);
 			}
 			vector3 getPosWorld()
 			{
-				return sqf::get_pos_world(_obj);
+				return sqf::get_pos_world(this->_obj);
 			}
 
 			bool isAlive()
 			{
-				return sqf::alive(_obj);
+				return sqf::alive(this->_obj);
 			}
 			void detach()
 			{
-				sqf::detach(_obj);
+				sqf::detach(this->_obj);
 			}
 			std::vector<float> actionIds()
 			{
@@ -85,27 +85,27 @@ namespace intercept
 			}
 			void attachTo(object obj, vector3 offset, r_string memPoint)
 			{
-				sqf::attach_to(_obj, obj, offset, memPoint);
+				sqf::attach_to(this->_obj, obj, offset, memPoint);
 			}
 			vector3 velocity()
 			{
-				return sqf::velocity(_obj);
+				return sqf::velocity(this->_obj);
 			}
 			r_string name()
 			{
-				return sqf::name(_obj);
+				return sqf::name(this->_obj);
 			}
 			r_string typeOf()
 			{
-				return sqf::type_of(_obj);
+				return sqf::type_of(this->_obj);
 			}
 			void inflame(bool burn)
 			{
-				sqf::inflame(_obj, burn);
+				sqf::inflame(this->_obj, burn);
 			}
 			bool inflamed()
 			{
-				return sqf::inflamed(_obj);
+				return sqf::inflamed(this->_obj);
 			}
 			void removeAction(float index)
 			{
@@ -117,51 +117,51 @@ namespace intercept
 			}
 			object objectParent()
 			{
-				return sqf::object_parent(_obj);
+				return sqf::object_parent(this->_obj);
 			}
 			std::vector<intercept::sqf::rv_container> everyContainer()
 			{
-				return sqf::every_container(_obj);
+				return sqf::every_container(this->_obj);
 			}
 			std::vector<object> everyBackpack()
 			{
-				return sqf::every_backpack(_obj);
+				return sqf::every_backpack(this->_obj);
 			}
 			float damage()
 			{
-				return sqf::damage(_obj);
+				return sqf::damage(this->_obj);
 			}
 			void setDamage(float damage)
 			{
-				sqf::set_damage(_obj, damage);
+				sqf::set_damage(this->_obj, damage);
 			}
 			float getHit(r_string selection)
 			{
-				return sqf::get_hit(_obj, selection);
+				return sqf::get_hit(this->_obj, selection);
 			}
 			float getHitpointDamage(r_string selection)
 			{
-				return sqf::get_hit_point_damage(_obj, selection);
+				return sqf::get_hit_point_damage(this->_obj, selection);
 			}
 			void setHit(r_string part, float damage, bool useEffects)
 			{
-				sqf::set_hit(_obj, part, damage);
+				sqf::set_hit(this->_obj, part, damage);
 			}
 			void setHitpointDamage(r_string selection, float damage, bool usEffects)
 			{
-				sqf::set_hit_point_damage(_obj, selection, damage);
+				sqf::set_hit_point_damage(this->_obj, selection, damage);
 			}
 			void allowDamage(bool allow)
 			{
-				sqf::allow_damage(_obj, allow);
+				sqf::allow_damage(this->_obj, allow);
 			}
 			bool isDamageAllowed()
 			{
-				return sqf::is_damage_allowed(_obj);
+				return sqf::is_damage_allowed(this->_obj);
 			}
 			bool local()
 			{
-				return sqf::local(_obj);
+				return sqf::local(this->_obj);
 			}
 		};
 	}
